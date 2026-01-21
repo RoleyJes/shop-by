59d557e528d98605a758f0bcdd1b836c1804726d
Jessica <jessicaimoesemi@gmail.com>
2026-01-21 21:51:35 +0100

    sign up ui

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,7 +1,42 @@
-<script setup></script>
+<script setup>
+import BreadCrumbs from "@/ui/BreadCrumbs.vue"
+import FormField from "@/ui/FormField.vue"
+import RegistrationLayout from "@/ui/RegistrationLayout.vue"
+
+const breadcrumbs = [
+  {
+    path: "/",
+    label: "Home",
+  },
+  {
+    label: "Account",
+  },
+]
+</script>
 
 <template>
-  <div></div>
+  <BreadCrumbs :breadcrumbs="breadcrumbs" />
+  <main>
+    <RegistrationLayout
+      header="Create Account"
+      description="Please register using account detail below."
+      buttonText="Create"
+      below-button-text="Return to Store"
+      below-button-path="/"
+    >
+      <div class="space-y-5">
+        <FormField
+          inputType="text"
+          placeholder="First Name"
+          label="First Name"
+          inputId="firstName"
+        />
+        <FormField inputType="text" placeholder="Last Name" label="Last Name" inputId="lastName" />
+        <FormField inputType="text" placeholder="Email" label="Email" inputId="email" />
+        <FormField inputType="text" placeholder="Password" label="Password" inputId="password" />
+      </div>
+    </RegistrationLayout>
+  </main>
 </template>
 
 <style lang="scss" scoped></style>
--- src/ui/FormField.vue
+++ src/ui/FormField.vue
@@ -1,9 +1,15 @@
 <template>
-  <input
-    :type="inputType"
-    :placeholder="placeholder"
-    class="inline-block w-full bg-white px-5 py-2 text-sm text-brand-primary placeholder:text-brand-primary focus:outline-none"
-  />
+  <div>
+    <label v-if="label" :for="inputId" class="mb-2 inline-block text-[15px]">{{ label }}</label>
+    <input
+      :type="inputType"
+      :placeholder="placeholder"
+      :class="[
+        'inline-block h-10 w-full bg-white py-2 text-sm text-brand-primary placeholder:text-brand-primary focus:outline-none',
+        label ? 'px-2.5' : 'px-5',
+      ]"
+    />
+  </div>
 </template>
 
 <script setup>
@@ -16,6 +22,14 @@ defineProps({
   placeholder: {
     type: String,
     default: "",
+  },
+  label: {
+    type: String,
+    required: false,
+  },
+  inputId: {
+    type: String,
+    required: false,
   },
 })
 </script>
